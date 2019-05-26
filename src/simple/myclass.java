1970bd7ea6f3f6e3178684970e893ac0054def7c
maneeshakalpani <maneshmkw@gmail.com>
2019-05-26 20:15:30 +0530

    first commit

---
+++ src/simple/myclass.java
@@ -0,0 +1,26 @@
+package simple;
+
+public class myclass {
+
+	public static void main(String[] args) {
+	
+		
+		int x=5+4;
+		if(x==5)
+		{
+			
+			
+			System.out.println("success");
+		}
+		else
+		{
+			
+			System.out.println("wrong");
+		}
+		
+		
+		
+
+	}
+
+}
